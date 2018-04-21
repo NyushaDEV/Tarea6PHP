0909c5b3e97bbc3a54c8f5f9844ebd197dedb2a9
Nouary <therox91@gmail.com>
2018-04-21 13:14:09 +0200

    Add class DB

---
+++ DB.php
@@ -0,0 +1,11 @@
+<?php
+
+class DB {
+    private $host = 'localhost';
+    private $user = 'root';
+    private $pass = '';
+    private $dbname = 'root';
+    private $pdo;
+
+    
+}
--- controller/controller.php
+++ controller/controller.php
@@ -1,5 +1,9 @@
 <?php 
 
 class Controlador {
+    private $vista;
     
+    public function __construct() {
+        require '../DB.php';
+    }
 }
