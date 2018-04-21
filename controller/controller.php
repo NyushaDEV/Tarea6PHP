7c17d644228b581249d3c0b0141ab0c8b3f2895f
Nouary <therox91@gmail.com>
2018-04-21 13:45:15 +0200

    login method

--- controller/controller.php
+++ controller/controller.php
@@ -14,16 +14,36 @@ class Controlador {
         $this->vistaLogin = new vistaLogin();
         
         
+        // Vistas
+            $this->vista->start();
+            $this->vistaLogin();
+            $this->vista->end();
         //
-        $this->vista->start();
-        $this->vistaLogin();
-        $this->vista->end();
-        //
+
+        if(isset($_GET['login'])) {
+            $this->login();
+        }
         
     }
 
 
     public function vistaLogin() {
         $this->vistaLogin->loginForm();
+    }
+
+    public function login() {
+
+        $json = array();
+        if(isset($_POST['usuario']) && isset($_POST['pass'])) {
+            $usuario = $_POST['usuario'];
+            $pass = $_POST['pass'];
+
+            if(empty($usuario) || empty($pass)) {
+                $json['mensaje'] = 'Los campos usuario o contraseña están vacios';
+            } else {
+            }
+
+            echo json_encode($json);
+        }
     }
 }
--- views/vistaLogin.php
+++ views/vistaLogin.php
@@ -6,7 +6,7 @@ class vistaLogin extends vista {
     public function loginForm() {
         echo 
         '<h1>Iniciar Sesión</h1>' .
-        '<form method="POST" action="">' .
+        '<form method="POST" action="?login">' .
             '<div><label>Usuario</label></div>' . 
             '<div><input type="text" name="usuario"></div>' . 
             '<div><label>Contraseña</label></div>' . 
